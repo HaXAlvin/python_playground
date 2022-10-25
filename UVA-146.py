3216e08f4350b9de59c66ab4a4808b8081ff1d2b
CHUN-YU, HSUEH <48896687+HaXAlvin@users.noreply.github.com>
2022-10-25 23:34:41 +0800

    remove if

--- UVA-146.py
+++ UVA-146.py
@@ -1,18 +1,15 @@
 # uva-146 ID Codes
 
-
 def find_next(data): # time cost: O(n) + O(n) = O(n)
     length = len(data)
     if length == 1:
         return None
-    for i in range(length-1,0,-1): # max O(n) i = len -> 0
+    for i in range(length-1,0,-1): # max O(n)
         if data[i-1] >= data[i]:
             continue
-        if i == length-1:
-            return data[:-2]+data[-1]+data[-2]
         bigger = i
-        for j in range(i+1,length): # max O(n) j = i -> len
-            if data[j] > data[i-1] and data[j] <= data[bigger]:
+        for j in range(i+1,length): # max O(n)
+            if data[bigger] >= data[j] > data[i-1]:
                 bigger = j
         return data[:i-1]+data[bigger]+data[:bigger:-1]+data[i-1]+data[bigger-1:i-1:-1]
     return None
